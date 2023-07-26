aa1ee09242d8b3286ef124b143c34c50e253fa5e
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 16:56:28 +0700

    userDB feature

--- UserDB.java
+++ UserDB.java
@@ -1,3 +1,17 @@
-public class UserDB {
+import java.util.List;
 
+public class UserDB {
+    public UserDB(List<userPOJO> userList) {
+        this.userList = userList;
+    }
+
+    private List<userPOJO> userList;
+
+    public List<userPOJO> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<userPOJO> userList) {
+        this.userList = userList;
+    }
 }
