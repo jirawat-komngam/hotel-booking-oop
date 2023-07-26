03917e8f2040318a705dd8887f665bda8759d00c
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 16:55:59 +0700

    create welcomeapp officer

---
+++ WelcomeAppOficer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class WelcomeAppOficer {
+    private Scanner ear;
+
+    
+    
+
+    
+
+
+
+    public void call() {
+        System.out.println("-Agoda 1980");
+        System.out.println("1 login");
+        System.out.println("2 register");
+
+    }
+}
