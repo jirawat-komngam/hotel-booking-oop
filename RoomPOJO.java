cc1c953bdc8acf1d19eacecce1000f0790e66e5a
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 20:52:45 +0700

    update roompojo

--- RoomPOJO.java
+++ RoomPOJO.java
@@ -1,7 +1,9 @@
 import java.time.ZonedDateTime;
+import java.util.Map;
 
 public class RoomPOJO {
-    public RoomPOJO(String roomType, Double roomPrice, ZonedDateTime roomAvailiabledates) {
+
+    public RoomPOJO(String roomType, Double roomPrice, Map<ZonedDateTime, Boolean> roomAvailiabledates) {
         this.roomType = roomType;
         this.roomPrice = roomPrice;
         this.roomAvailiabledates = roomAvailiabledates;
@@ -9,7 +11,7 @@ public class RoomPOJO {
 
     private String roomType;
     private Double roomPrice;
-    private ZonedDateTime roomAvailiabledates;
+    private Map<ZonedDateTime, Boolean> roomAvailiabledates;
 
     public String getRoomType() {
         return roomType;
@@ -27,11 +29,12 @@ public class RoomPOJO {
         this.roomPrice = roomPrice;
     }
 
-    public ZonedDateTime getRoomAvailiabledates() {
+    public Map<ZonedDateTime, Boolean> getRoomAvailiabledates() {
         return roomAvailiabledates;
     }
 
-    public void setRoomAvailiabledates(ZonedDateTime roomAvailiabledates) {
+    public void setRoomAvailiabledates(Map<ZonedDateTime, Boolean> roomAvailiabledates) {
         this.roomAvailiabledates = roomAvailiabledates;
     }
+
 }
