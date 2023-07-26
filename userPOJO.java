a64ec04ab78b9bd943050de221a2f8745c28e9a8
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 16:47:16 +0700

    addfeature userPOJO

--- userPOJO.java
+++ userPOJO.java
@@ -1,3 +1,33 @@
-public class userPOJO {
+import java.util.List;
+import java.util.Map;
 
+public class userPOJO {
+    public userPOJO(Map<String, String> userInfo, String fullName, List<Booking> booking) {
+        this.userInfo = userInfo;
+        this.fullName = fullName;
+        this.booking = booking;
+    }
+    private Map<String, String> userInfo;
+    private String fullName;
+    private List<Booking> booking;
+    public Map<String, String> getUserInfo() {
+        return userInfo;
+    }
+    public void setUserInfo(Map<String, String> userInfo) {
+        this.userInfo = userInfo;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public List<Booking> getBooking() {
+        return booking;
+    }
+    public void setBooking(List<Booking> booking) {
+        this.booking = booking;
+    }
+
+    
 }
