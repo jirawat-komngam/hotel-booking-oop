91355512506efc2f8f9ddc23295ea3b1578fea35
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 16:47:37 +0700

    add feature bookingpojo

---
+++ Booking.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+
+public class Booking {
+    private String city;
+    private String hotelName;
+    private String fullName;
+    private String hotelType;
+    private ZonedDateTime checkInTime;
+    private ZonedDateTime checkOutTime;
+}
