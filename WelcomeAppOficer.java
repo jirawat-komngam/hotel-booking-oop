b15165f4dfb8f79b9926f43df35df61f897d692d
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 17:06:51 +0700

    add feature to welcomeapp

--- WelcomeAppOficer.java
+++ WelcomeAppOficer.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class WelcomeAppOficer implements Officer {
     private Scanner ear;
+    private RegisterOfficer registerOfficer;
 
-    WelcomeAppOficer() {
+    WelcomeAppOficer(RegisterOfficer realRegisterOfficer) {
         ear = new Scanner(System.in);
+        registerOfficer = realRegisterOfficer;
     }
 
     public void call() {
         System.out.println("-Agoda 1980");
         System.out.println("1 login");
         System.out.println("2 register");
+        String userInput = ear.nextLine();
+        dicision(userInput);
     }
 
     public void dicision(String userword) {
         if (userword.equals("1")) {
             System.out.println("login");
         } else if (userword.equals("2")) {
-            System.out.println("login");
+            registerOfficer.call();
         }
     }
 }
