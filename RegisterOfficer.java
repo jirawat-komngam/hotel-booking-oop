52d87da0f4f799896c0fe4848ed8141eab1d16c1
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 17:07:10 +0700

    add feature to register

--- RegisterOfficer.java
+++ RegisterOfficer.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class RegisterOfficer implements Officer {
+    private Scanner ear;
+
+    RegisterOfficer() {
+        ear = new Scanner(System.in);
+    }
+
     public void call() {
+        System.out.println("register");
+        System.out.println("-username");
+        String userName = ear.nextLine();
+        System.out.println("-fullname");
+        String fullName = ear.nextLine();
+        System.out.println("password");
+        String password = ear.nextLine();
+        System.out.println("confirm password");
+        String confirmPassword = ear.nextLine();
+        if (password.equals(confirmPassword)) {
+            System.out.println("register success");
+        } else {
+            System.out.println("your confirm password is incorect");
+        }
     }
 }
