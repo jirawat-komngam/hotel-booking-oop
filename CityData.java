8237995562e9788b6b217d1e18f3122c8ed4b1d0
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 20:31:41 +0700

    update hotelpojo and db

--- CityData.java
+++ CityData.java
@@ -1,7 +1,37 @@
 import java.util.List;
 
 public class CityData {
+    public CityData(String cityName, String country, List<HotelPOJO> hotelList) {
+        this.cityName = cityName;
+        this.country = country;
+        this.hotelList = hotelList;
+    }
+
     private String cityName;
     private String country;
-    private List hotelList;
+    private List<HotelPOJO> hotelList;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<HotelPOJO> getHotelList() {
+        return hotelList;
+    }
+
+    public void setHotelList(List<HotelPOJO> hotelList) {
+        this.hotelList = hotelList;
+    }
 }
