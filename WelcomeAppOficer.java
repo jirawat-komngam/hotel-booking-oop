fcc762a9d9047b78a75db770a038d7a47c5d3b55
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-26 16:57:53 +0700

    add feature welcomeapp

--- WelcomeAppOficer.java
+++ WelcomeAppOficer.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class WelcomeAppOficer {
     private Scanner ear;
 
-    
-    
-
-    
-
-
+    WelcomeAppOficer() {
+        ear = new Scanner(System.in);
+    }
 
     public void call() {
         System.out.println("-Agoda 1980");
         System.out.println("1 login");
         System.out.println("2 register");
+    }
+
+    public void dicision(String userword) {
 
     }
 }
